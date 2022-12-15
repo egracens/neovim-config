c69c4d18e30e2f7c964874f3df6929b115e13e52
Anton Nikiforov <anton.ni@kodolabs.com>
2022-12-16 01:55:00 +0300

    Setup rust tools

--- plugin_configs/lsp.lua
+++ plugin_configs/lsp.lua
@@ -89,3 +89,50 @@ require('lspconfig').sumneko_lua.setup{
     }
   }
 }
+
+-- =================== RUST ==================
+-- Configure LSP through rust-tools.nvim plugin.
+-- rust-tools will configure and enable certain LSP features for us.
+-- See https://github.com/simrat39/rust-tools.nvim#configuration
+local rustoyts = {
+  tools = {
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
+  },
+
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+  server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = on_attach,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+  },
+}
+
+local format_sync_grp = vim.api.nvim_create_augroup("Format", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.rs",
+  callback = function()
+    vim.lsp.buf.format({ timeout_ms = 200 })
+  end,
+  group = format_sync_grp,
+})
+
+require("rust-tools").setup(rustOpts)
--- plugins.lua
+++ plugins.lua
@@ -1,4 +1,17 @@
-return require('packer').startup(function(use)
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- Theme
@@ -39,6 +52,10 @@ return require('packer').startup(function(use)
   -- Go integration
   use 'fatih/vim-go'
 
+  -- Rust integration
+  use 'rust-lang/rust.vim'
+  use 'simrat39/rust-tools.nvim'
+
   -- Test helpers
   use 'vim-test/vim-test'
 
@@ -68,3 +85,9 @@ return require('packer').startup(function(use)
   use 'L3MON4D3/LuaSnip'
   use 'saadparwaiz1/cmp_luasnip'
 end)
+
+-- the first run will install packer and our plugins
+if packer_bootstrap then
+  require("packer").sync()
+  return
+end
